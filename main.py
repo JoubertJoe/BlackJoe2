73293513e89bc8bd9c6642d19043c3c5e41b47c4
Joubert Lellis <joubert.lellis@gmail.com>
2025-03-25 12:38:46 -0300

    Criando Baralho e outras classes iniciais
    Signed-off-by: Joubert Lellis <joubert.lellis@gmail.com>

--- main.py
+++ main.py
@@ -2,25 +2,6 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-import pyCardDeck
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-    my_deck = pyCardDeck.Deck(cards=[1, 2, 3], name='My Awesome Deck')
-
-    my_deck.shuffle()
-
-    card = my_deck.draw()
-
-    print(card)
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+print("Main Test")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
